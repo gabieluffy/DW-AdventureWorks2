4571fbf24a754dd06fa18ae34c57f1352863b681
Gabrielly Gervasio <131404044+gabieluffy@users.noreply.github.com>
2026-06-30 15:19:04 -0300

    Update 09_receita_categoria.sql

--- indicadores/09_receita_categoria.sql
+++ indicadores/09_receita_categoria.sql
@@ -1,7 +1,13 @@
 SELECT
     p.categoria,
-    SUM(f.valor_total) AS receita
+    COUNT(*) AS total_vendas,
+    SUM(f.quantidade) AS quantidade_vendida,
+    SUM(f.receita) AS receita_total,
+    SUM(f.lucro) AS lucro_total
 FROM dw.fato_vendas f
-JOIN dw.dim_produto p ON f.sk_produto = p.sk_produto
-GROUP BY p.categoria
-ORDER BY receita DESC;
+INNER JOIN dw.dim_produto p
+    ON p.sk_produto = f.sk_produto
+GROUP BY
+    p.categoria
+ORDER BY
+    receita_total DESC;

64147a58380221e99d4181d7340b875424e32956
Gabrielly Gervasio <131404044+gabieluffy@users.noreply.github.com>
2026-06-30 15:17:47 -0300

    Update 04_receita_por_ano.sql

--- indicadores/04_receita_por_ano.sql
+++ indicadores/04_receita_por_ano.sql
@@ -1,7 +1,10 @@
 SELECT
     t.ano,
-    SUM(f.valor_total) AS receita
+    COUNT(*) AS total_vendas,
+    SUM(f.receita) AS receita_total,
+    SUM(f.lucro) AS lucro_total
 FROM dw.fato_vendas f
-JOIN dw.dim_tempo t ON f.sk_tempo = t.sk_tempo
+INNER JOIN dw.dim_tempo t
+    ON t.sk_tempo = f.sk_tempo
 GROUP BY t.ano
 ORDER BY t.ano;

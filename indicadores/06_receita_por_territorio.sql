3fe6e014bcf2d0b9daf70687148483b4195ed33f
Gabrielly Gervasio <131404044+gabieluffy@users.noreply.github.com>
2026-06-30 15:18:18 -0300

    Update 06_receita_por_territorio.sql

--- indicadores/06_receita_por_territorio.sql
+++ indicadores/06_receita_por_territorio.sql
@@ -1,7 +1,13 @@
 SELECT
     te.nome_territorio,
-    SUM(f.valor_total) AS receita
+    COUNT(*) AS total_vendas,
+    SUM(f.receita) AS receita_total,
+    SUM(f.lucro) AS lucro_total,
+    ROUND(AVG(f.receita),2) AS ticket_medio
 FROM dw.fato_vendas f
-JOIN dw.dim_territorio te ON f.sk_territorio = te.sk_territorio
-GROUP BY te.nome_territorio
-ORDER BY receita DESC;
+INNER JOIN dw.dim_territorio te
+    ON te.sk_territorio = f.sk_territorio
+GROUP BY
+    te.nome_territorio
+ORDER BY
+    receita_total DESC;

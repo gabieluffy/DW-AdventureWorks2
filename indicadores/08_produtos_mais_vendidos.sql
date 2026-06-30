76d97dc5a9dde1efe775e450afd6017a06fcb4ba
Gabrielly Gervasio <131404044+gabieluffy@users.noreply.github.com>
2026-06-30 15:18:47 -0300

    Update 08_produtos_mais_vendidos.sql

--- indicadores/08_produtos_mais_vendidos.sql
+++ indicadores/08_produtos_mais_vendidos.sql
@@ -1,7 +1,15 @@
 SELECT
     p.nome_produto,
-    SUM(f.quantidade) AS quantidade_vendida
+    SUM(f.quantidade) AS quantidade_vendida,
+    SUM(f.receita) AS receita_total,
+    ROUND(
+        SUM(f.receita) / NULLIF(SUM(f.quantidade),0),
+        2
+    ) AS preco_medio
 FROM dw.fato_vendas f
-JOIN dw.dim_produto p ON f.sk_produto = p.sk_produto
-GROUP BY p.nome_produto
-ORDER BY quantidade_vendida DESC;
+INNER JOIN dw.dim_produto p
+    ON p.sk_produto = f.sk_produto
+GROUP BY
+    p.nome_produto
+ORDER BY
+    quantidade_vendida DESC;

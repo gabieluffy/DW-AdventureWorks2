fdce38a5ba9f3df37a4183c4de797745886d62f2
Gabrielly Gervasio <131404044+gabieluffy@users.noreply.github.com>
2026-06-30 15:16:53 -0300

    Update 01_receita_total.sql

--- indicadores/01_receita_total.sql
+++ indicadores/01_receita_total.sql
@@ -1,3 +1,5 @@
 SELECT
-    SUM(valor_total) AS receita_total
-FROM dw.fato_vendas;
+    COUNT(*) AS total_registros,
+    SUM(f.receita) AS receita_total,
+    AVG(f.receita) AS media_por_venda
+FROM dw.fato_vendas f;

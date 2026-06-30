f68acd5db9e6a85a1a5efcb0a4affb0dca20773f
Gabrielly Gervasio <131404044+gabieluffy@users.noreply.github.com>
2026-06-30 15:19:19 -0300

    Update 10_total_descontos.sql

--- indicadores/10_total_descontos.sql
+++ indicadores/10_total_descontos.sql
@@ -1,3 +1,12 @@
 SELECT
-    SUM(desconto) AS total_descontos
-FROM dw.fato_vendas;
+    SUM(f.desconto) AS total_descontos,
+    SUM(f.receita) AS receita_liquida,
+    SUM(f.receita + f.desconto) AS receita_bruta,
+    ROUND(
+        (
+            SUM(f.desconto) /
+            NULLIF(SUM(f.receita + f.desconto),0)
+        ) * 100,
+        2
+    ) AS percentual_desconto
+FROM dw.fato_vendas f;

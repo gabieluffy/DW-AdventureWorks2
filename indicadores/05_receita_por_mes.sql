6640c8d1cefdadb8398fd4f8d084105dbde27246
Gabrielly Gervasio <131404044+gabieluffy@users.noreply.github.com>
2026-06-30 15:18:02 -0300

    Update 05_receita_por_mes.sql

--- indicadores/05_receita_por_mes.sql
+++ indicadores/05_receita_por_mes.sql
@@ -1,8 +1,15 @@
 SELECT
     t.ano,
     t.mes,
-    SUM(f.valor_total) AS receita
+    COUNT(*) AS total_vendas,
+    SUM(f.receita) AS receita_total,
+    ROUND(AVG(f.receita),2) AS ticket_medio
 FROM dw.fato_vendas f
-JOIN dw.dim_tempo t ON f.sk_tempo = t.sk_tempo
-GROUP BY t.ano, t.mes
-ORDER BY t.ano, t.mes;
+INNER JOIN dw.dim_tempo t
+    ON t.sk_tempo = f.sk_tempo
+GROUP BY
+    t.ano,
+    t.mes
+ORDER BY
+    t.ano,
+    t.mes;
